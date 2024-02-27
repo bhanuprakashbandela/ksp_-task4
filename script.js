a7a32de9828cfab9dcd730c27c0940d81a850d8b
bhanuprakashbandela <160694813+bhanuprakashbandela@users.noreply.github.com>
2024-02-27 11:42:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("hello world") 
+
+
+//variable
+
+let number1 = 5;
+let number2 = 2;
+let number3 = 7;
+
+console.log(number1);
+
+// ver let const
+
+// var - let are both are same 
+
+let number4 = 66;
+let number5 = 88;
+
+console.log(number4);
+
+//in let we declare the same variable name it prints second variable date . the data will updated
+
+let number6 = 66;
+ number6 = 88;
+
+console.log(number6);
+
+// const
+// in const we cont re-declare the same variable name 
+
+const number7 = 33;
+const number8 = 55;
+
+console.log(number8);
+
+//data type
+let number9 = 22;  //ineger data type
+
+let number10 = 33.4; //float 
+
+let name1 = "ramu"; //string
+
+let char = "r"; //character
+
+let bool = true; //boolean data type  in true-false
+
+console.log(typeof(name1));
+
+
+
+
+
+
+
+
